ef753f34c26449b1e436e90c096f9faea87994a1
Aarish <aarishhussain62@gmail.com>
2022-08-12 20:58:06 +0530

    updated

--- src/components/legalInformation/index.js
+++ src/components/legalInformation/index.js
@@ -6,7 +6,7 @@ import LegalInformation from "../legalInformation/LegalInformation";
 export default function index() {
   return (
     <div className="App" >
-      <div className="container">
+      <div className="">
        <Header />
         <LegalInformation />
         <Footer />
